5bd602f20f386e10d514db3763ec2769f8839c13
smdewi <smdewi2020@gmail.com>
2022-11-21 20:29:15 +0800

    build menu page

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+
+const PORT = 3000;
+
+let app = express();
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+app.get('/menu', (req, res) => {
+    res.render("menu");
+});
+
+
+
+app.listen(PORT, () => console.log("Server started"));
